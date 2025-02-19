6dfaefcf4f0def1ef366ea2656ef7398dc8e2367
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2025-02-19 10:30:47 +0300

    handle errors using exceptions

--- include/MemoryMap.hpp
+++ include/MemoryMap.hpp
@@ -6,14 +6,14 @@ namespace utils {
 
 class MapDeleter {
 public:
-    void operator()(std::byte* mapped);
+    void operator()(std::byte* mapped) noexcept;
 
 private:
     MapDeleter() = default;
     MapDeleter(int fd, size_t fileSize);
 
-    int m_fileDescriptor;
-    size_t m_mapLength;
+    int m_fileDescriptor = -1;
+    size_t m_mapLength = 0;
 
     friend std::unique_ptr<std::byte[], MapDeleter> MapFile(const std::filesystem::path& path);
 };
--- src/MemoryMap.cpp
+++ src/MemoryMap.cpp
@@ -2,12 +2,23 @@
 #include <sys/mman.h>
 #include <unistd.h>
 
+#include <cerrno>
+#include <system_error>
+
 #include "Defer.hpp"
 #include "MemoryMap.hpp"
 
+namespace {
+
+inline void errnoThrow(const char* what) {
+    throw std::system_error(errno, std::system_category(), what);
+}
+
+}// namespace
+
 namespace utils {
 
-void MapDeleter::operator()(std::byte* mapped) {
+void MapDeleter::operator()(std::byte* mapped) noexcept {
     munmap(mapped, m_mapLength);
     close(m_fileDescriptor);
 }
@@ -16,18 +27,18 @@ MapDeleter::MapDeleter(int fd, size_t fileSize): m_fileDescriptor(fd), m_mapLeng
 
 std::unique_ptr<std::byte[], MapDeleter> MapFile(const std::filesystem::path& path) {
     int fd = open(std::filesystem::absolute(path).c_str(), 0, O_RDWR);
-    if (fd == -1) return {nullptr, MapDeleter()};
+    if (fd == -1) errnoThrow("could not open file to map");
     Defer closeFd([fd]() { close(fd); });
 
     off64_t end = lseek64(fd, 0, SEEK_END);
-    if (end == -1) return {nullptr, MapDeleter()};
+    if (end == -1) errnoThrow("could not find file end");
     off64_t start = lseek64(fd, 0, SEEK_SET);
-    if (start == -1) return {nullptr, MapDeleter()};
+    if (start == -1) errnoThrow("could not find file start");
 
     auto fileSize = static_cast<size_t>(end - start);
     auto mapped =
-            static_cast<std::byte*>(mmap(nullptr, fileSize, PROT_READ | PROT_WRITE, MAP_FILE | MAP_SHARED, fd, 0));
-    if (mapped == MAP_FAILED) return {nullptr, MapDeleter()};
+            static_cast<std::byte*>(mmap(nullptr, fileSize, PROT_READ | PROT_WRITE, MAP_FILE | MAP_PRIVATE, fd, 0));
+    if (mapped == MAP_FAILED) errnoThrow("could not map file to memory");
 
     closeFd.cancel();
     return {mapped, MapDeleter(fd, fileSize)};
