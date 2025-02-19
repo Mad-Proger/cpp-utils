a7b714b3db75d5312b712fce941c8c0cba83a2da
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2025-02-19 13:26:10 +0300

    move mapping to wrapper class

--- include/MemoryMap.hpp
+++ include/MemoryMap.hpp
@@ -1,23 +1,40 @@
 #pragma once
 
+#include <cstddef>
+#include <cstdint>
 #include <filesystem>
+#include <span>
 
 namespace utils {
 
-class MapDeleter {
+class FileMapping {
 public:
-    void operator()(std::byte* mapped) noexcept;
+    FileMapping();
+    explicit FileMapping(const std::filesystem::path& path);
+
+    FileMapping(FileMapping&&) noexcept;
+    FileMapping& operator=(FileMapping&&) noexcept;
+
+    class MapDeleter {
+    public:
+        void operator()(uint8_t* mapped) noexcept;
+
+    private:
+        MapDeleter() = default;
+        MapDeleter(int fd, size_t fileSize);
+
+        int m_fileDescriptor = -1;
+        size_t m_mapLength = 0;
+
+        friend class FileMapping;
+    };
+
+    std::span<uint8_t> getBytes() const noexcept;
+    void swap(FileMapping& other) noexcept;
 
 private:
-    MapDeleter() = default;
-    MapDeleter(int fd, size_t fileSize);
-
-    int m_fileDescriptor = -1;
-    size_t m_mapLength = 0;
-
-    friend std::unique_ptr<std::byte[], MapDeleter> MapFile(const std::filesystem::path& path);
+    size_t m_fileSize;
+    std::unique_ptr<uint8_t[], MapDeleter> m_mapping;
 };
-
-std::unique_ptr<std::byte[], MapDeleter> MapFile(const std::filesystem::path& path);
 
 }// namespace utils
--- src/MemoryMap.cpp
+++ src/MemoryMap.cpp
@@ -1,4 +1,6 @@
+#include <cstdint>
 #include <fcntl.h>
+#include <filesystem>
 #include <sys/mman.h>
 #include <unistd.h>
 
@@ -18,14 +20,16 @@ inline void errnoThrow(const char* what) {
 
 namespace utils {
 
-void MapDeleter::operator()(std::byte* mapped) noexcept {
+void FileMapping::MapDeleter::operator()(uint8_t* mapped) noexcept {
     munmap(mapped, m_mapLength);
     close(m_fileDescriptor);
 }
 
-MapDeleter::MapDeleter(int fd, size_t fileSize): m_fileDescriptor(fd), m_mapLength(fileSize) {}
+FileMapping::MapDeleter::MapDeleter(int fd, size_t fileSize): m_fileDescriptor(fd), m_mapLength(fileSize) {}
 
-std::unique_ptr<std::byte[], MapDeleter> MapFile(const std::filesystem::path& path) {
+FileMapping::FileMapping(): m_fileSize(0), m_mapping{nullptr, MapDeleter()} {}
+
+FileMapping::FileMapping(const std::filesystem::path& path): FileMapping() {
     int fd = open(std::filesystem::absolute(path).c_str(), 0, O_RDWR);
     if (fd == -1) errnoThrow("could not open file to map");
     Defer closeFd([fd]() { close(fd); });
@@ -35,12 +39,33 @@ std::unique_ptr<std::byte[], MapDeleter> MapFile(const std::filesystem::path& pa
     off64_t start = lseek64(fd, 0, SEEK_SET);
     if (start == -1) errnoThrow("could not find file start");
 
-    auto fileSize = static_cast<size_t>(end - start);
-    auto mapped = static_cast<std::byte*>(mmap(nullptr, fileSize, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, start));
+    m_fileSize = static_cast<size_t>(end - start);
+    auto mapped = static_cast<uint8_t*>(mmap(nullptr, m_fileSize, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, start));
     if (mapped == MAP_FAILED) errnoThrow("could not map file to memory");
 
     closeFd.cancel();
-    return {mapped, MapDeleter(fd, fileSize)};
+    m_mapping = std::unique_ptr<uint8_t[], MapDeleter>{mapped, MapDeleter(fd, m_fileSize)};
+}
+
+FileMapping::FileMapping(FileMapping&& other) noexcept
+    : m_fileSize(other.m_fileSize)
+    , m_mapping(std::move(other.m_mapping)) {
+    other.m_fileSize = 0;
+}
+
+FileMapping& FileMapping::operator=(FileMapping&& other) noexcept {
+    FileMapping moved = std::move(other);
+    swap(moved);
+    return *this;
+}
+
+std::span<uint8_t> FileMapping::getBytes() const noexcept {
+    return {m_mapping.get(), m_fileSize};
+}
+
+void FileMapping::swap(FileMapping& other) noexcept {
+    std::swap(m_fileSize, other.m_fileSize);
+    std::swap(m_mapping, other.m_mapping);
 }
 
 }// namespace utils
