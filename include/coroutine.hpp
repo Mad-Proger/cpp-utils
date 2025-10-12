7d4b681280cfd82eae226e326302804660c2e176
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2025-10-12 23:59:45 +0300

    fix handling of throwing body copy constructor

--- include/coroutine.hpp
+++ include/coroutine.hpp
@@ -56,17 +56,19 @@ inline Coroutine::Coroutine(Body&& body): m_stack{AllocateStack()}
     } trampoline_data{body, *this};
     SetupStack(&trampoline_data, [](uint8_t* old_stack, void* data_raw) {
         auto data = reinterpret_cast<TrampolineData*>(data_raw);
-        auto func = std::forward<Body>(data->body);
         Coroutine& coro = data->coro;
-
         coro.m_target_stack = old_stack;
-        coro.Switch();
+
         try {
+            auto func = std::forward<Body>(data->body);
+            coro.Switch();
             std::invoke(std::move(func), Handle{coro});
         } catch (...) {
             coro.m_exception = std::current_exception();
         }
+
         coro.m_finished = true;
         return coro.m_target_stack;
     });
+    if (m_exception) std::rethrow_exception(m_exception);
 }
