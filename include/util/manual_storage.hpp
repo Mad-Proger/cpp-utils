ff030b469e22236a2d8052ebe17df8349fd472b4
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2025-12-09 17:32:23 +0300

    implement ManualStorage utility

---
+++ include/util/manual_storage.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <concepts>
+#include <cstddef>
+#include <memory>
+#include <new>
+#include <type_traits>
+#include <utility>
+
+namespace util {
+
+template <typename T>
+class ManualStorage {
+public:
+    ManualStorage() noexcept = default;
+
+    ManualStorage(const ManualStorage&) = delete;
+    ManualStorage& operator=(const ManualStorage&) = delete;
+    ManualStorage(ManualStorage&&) = delete;
+    ManualStorage& operator=(ManualStorage&&) = delete;
+
+    template <typename... Args>
+        requires std::constructible_from<T, Args...>
+    void Emplace(Args&&... args) noexcept(std::is_nothrow_constructible_v<T, Args...>) {
+        std::construct_at(GetPointer(), std::forward<Args>(args)...);
+    }
+
+    void Destroy() noexcept(std::is_nothrow_destructible_v<T>) {
+        std::destroy_at(GetPointer());
+    }
+
+    T Take() noexcept(std::is_nothrow_move_constructible_v<T> && std::is_nothrow_destructible_v<T>) {
+        T obj = std::move(Get());
+        Destroy();
+        return obj;
+    }
+
+    T& Get() noexcept {
+        return *GetPointer();
+    }
+
+    const T& Get() const noexcept {
+        return *GetPointer();
+    }
+
+    T& operator*() noexcept {
+        return Get();
+    }
+
+    const T& operator*() const noexcept {
+        return Get();
+    }
+
+    T* operator->() noexcept {
+        return GetPointer();
+    }
+
+    const T* operator->() const noexcept {
+        return GetPointer();
+    }
+
+private:
+    T* GetPointer() noexcept {
+        return std::launder(reinterpret_cast<T*>(m_objectStorage));
+    }
+
+    const T* GetPointer() const noexcept {
+        return std::launder(reinterpret_cast<const T*>(m_objectStorage));
+    }
+
+private:
+    alignas(T) std::byte m_objectStorage[sizeof(T)];
+};
+
+}// namespace util
