5f9911b6e6494b2e27141dda660d9125417daa17
Mikhail Zaytsev <m.zaytsev.23@gmail.com>
2026-01-06 18:09:46 +0300

    add basic InvocableRef tests

--- include/util/invocable_ref.hpp
+++ include/util/invocable_ref.hpp
@@ -17,25 +17,60 @@ public:
         requires std::is_invocable_r_v<R, F&, Args...>
     explicit InvocableRef(F& func) noexcept
         : m_call_thunk{[](void* func_body, Args... args) -> R {
-            return std::invoke(*reinterpret_cast<F*>(func_body), std::move(args)...);
+            return std::invoke(*reinterpret_cast<F*>(func_body), std::forward<Args>(args)...);
         }}
         , m_data{std::addressof(func)} {}
 
     explicit InvocableRef(R (*fptr)(Args...)) noexcept
-        : m_call_thunk{[](R (*fptr)(Args...), Args... args) { return fptr(std::move(args)...); }}
+        : m_call_thunk{[](void* fptr_raw, Args... args) {
+            auto fptr = static_cast<R (*)(Args...)>(fptr_raw);
+            return fptr(std::forward<Args>(args)...);
+        }}
         , m_data{fptr} {}
 
     R operator()(Args... args) {
-        return m_call_thunk(m_data, std::move(args)...);
+        return m_call_thunk(m_data, std::forward<Args>(args)...);
     }
 
-    void Swap(InvocableRef& other) {
+    void Swap(InvocableRef& other) noexcept {
         std::swap(m_call_thunk, other.m_call_thunk);
         std::swap(m_data, other.m_data);
     }
 
 private:
     R (*m_call_thunk)(void*, Args...);
+    void* m_data;
+};
+
+template <typename R, typename... Args>
+class InvocableRef<R(Args...) noexcept> {
+public:
+    template <typename F>
+        requires std::is_nothrow_invocable_r_v<R, F&, Args...>
+    explicit InvocableRef(F& func) noexcept
+        : m_call_thunk{[](void* func_body, Args... args) noexcept -> R {
+            return std::invoke(*static_cast<F*>(func_body), std::forward<Args>(args)...);
+        }}
+        , m_data{std::addressof(func)} {}
+
+    explicit InvocableRef(R (*fptr)(Args...) noexcept) noexcept
+        : m_call_thunk{[](void* fptr_raw, Args... args) {
+            auto fptr = static_cast<R (*)(Args...) noexcept>(fptr_raw);
+            return fptr(std::forward<Args>(args)...);
+        }}
+        , m_data{fptr} {}
+
+    R operator()(Args... args) noexcept {
+        return m_call_thunk(m_data, std::forward<Args>(args)...);
+    }
+
+    void Swap(InvocableRef& other) noexcept {
+        std::swap(m_call_thunk, other.m_call_thunk);
+        std::swap(m_data, other.m_data);
+    }
+
+private:
+    R (*m_call_thunk)(void*, Args...) noexcept;
     void* m_data;
 };
 
